2144cf10423fa9b5961342f1b59034be516e91ac
Sushi <71588514+projetuser@users.noreply.github.com>
2024-12-03 10:28:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,20 +24,17 @@ def encryptage(valeur, cle):
     except Exception as e:
         return str(e), 500
 
-@app.route('/decrypt/<string:token>/<string:cle>')
-def decryptage(token, cle):
+@app.route('/encrypt/<string:valeur>/<string:cle>')
+def encryptage(valeur, cle):
     try:
         # Validation de la clé
         if len(cle) != 44 or not base64.urlsafe_b64decode(cle.encode()):
             return "Clé invalide. Elle doit être une chaîne de 32 octets encodée en base64.", 400
         
         f = Fernet(cle.encode())  # Utiliser la clé fournie
-        token_bytes = token.encode()  # Conversion str -> bytes
-        valeur_bytes = f.decrypt(token_bytes)  # Décryptage
-        valeur = valeur_bytes.decode()  # Conversion bytes -> str
-        return f"Valeur décryptée : {valeur}"  # Retourne la valeur décryptée
-    except InvalidToken:
-        return "Token invalide ou clé incorrecte.", 400
+        valeur_bytes = valeur.encode()  # Conversion str -> bytes
+        token = f.encrypt(valeur_bytes)  # Encrypt la valeur
+        return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
     except Exception as e:
         return str(e), 500
 
